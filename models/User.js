3b06131236a9d5ed9ae1620db084120b8431815e
KJH <dev.jihyun@gmail.com>
2021-02-23 23:29:56 +0900

    암호화 기능 추가

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose =require('mongoose');
-
+const bycrypt= require('bcrypt');
+const saltRounds = 10
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -30,6 +31,21 @@ const userSchema = mongoose.Schema({
         type:Number
     }
 })
-
+//저장하기 전에 
+userSchema.pre('save',function(next){
+    var user = this;
+    //비밀번호를 암호화 시킨다.
+    if(user.isModified('password')){
+    bycrypt.genSalt(saltRounds,function(err,salt){
+        if(err) return next(err)
+        bycrypt.hash(user.password,salt,function(err,hash){
+            if(err) return next(err)
+            user.password = hash
+            next()
+        })
+    })
+    }
+})
+ 
 const User = mongoose.model('User',userSchema)
 module.exports={ User }
