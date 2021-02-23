c60a9ebcef5e5e7acd4a789f364b0b3a1bbe0bee
KJH <dev.jihyun@gmail.com>
2021-02-23 22:25:31 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require('express')
 const app=express()
 const port = 5000
 const mongoose=require('mongoose')
+const {User}= require("./models/User")
+const bodyParser = require('body-parser')
+const config = require('./config/key')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
-mongoose.connect('mongodb+srv://jh:8665@cluster0.wti5a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
 }).then(()=> console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
+
+//몽고 디비 연결
 app.get('/',(req,res)=> res.send('Hello World!'))
 
+app.post('/register',(req,res)=>{
+    const user= new User(req.body)
+    user.save((err,userInfo)=>{
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 app.listen(port,() => console.log(`Example app listening on port ${port}!`))
